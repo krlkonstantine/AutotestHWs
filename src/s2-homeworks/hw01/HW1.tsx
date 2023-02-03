cee5c60233d6c168c89ddd4a84b08d2d81ba998b
krlkonstantine <kostea.023@gmail.com>
2023-02-03 13:27:00 +0300

    small changes 3

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -52,7 +52,7 @@ export const friendMessage0: MessageType = {
 const HW1 = () => {
     return (
         <div id={'hw1'}>
-            <div className={s2.hwTitle}>Homework #1</div>
+            <div className={s2.hw1Title}>Homework #1</div>
             <div className={s2.hw}>
                 <div className={s2.messagesContainer}>
                     <Message message={message0}/>
