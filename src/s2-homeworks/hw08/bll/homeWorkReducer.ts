7faf85acfe8bb576cc4f54c8d3849efdc408252d
krlkonstantine <kostea.023@gmail.com>
2023-02-07 07:57:17 +0300

    reducer fixed, test for reducer passed

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,64 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        /*case 'sort': { // by name
+            let sortedState: UserType[] = state.sort(function (a, b) {
+                if (action.payload === 'up') {
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    return 0;
+                }
+                if (action.payload === 'down') {
+                    if (a.name > b.name) {
+                        return -1
+                    }
+                    if (a.name < b.name) {
+                        return 1
+                    }
+                    return 0
+                }
+            })
+            return sortedState
+        }*/
+        case 'sort': {
+            if (action.payload === 'up') {
+                let sortedState: UserType[] = state.sort(function (a, b) {
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    return 0;
+                })
+                return sortedState
+            }
+            if (action.payload === 'down') {
+                let sortedState: UserType[] = state.sort(function (a, b) {
+                    if (a.name < b.name) {
+                        return 1;
+                    }
+                    if (a.name > b.name) {
+                        return -1;
+                    }
+                    return 0;
+                })
+                return sortedState
+            }
+            return state
         }
-        case 'check': {
 
-            return state // need to fix
+        case 'check': {
+            return state.filter(usr => usr.age >= 18) // need to fix
         }
         default:
             return state
+
     }
 }
